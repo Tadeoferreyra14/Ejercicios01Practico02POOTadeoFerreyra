f6a353a424727bcdea1858a36acd7638945aa32d
Tadeoferreyra14 <tadeoferreyra14@gmail.com>
2021-03-15 15:55:17 -0300

    Initial commit

---
+++ Ejercicios01Practico02/Program.cs
@@ -0,0 +1,47 @@
+﻿using Ejercicios01Practico02POO.BL;
+using System;
+
+namespace Ejercicios01Practico02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Rectangulo rectangulo = new Rectangulo();
+                Console.WriteLine("Ingrese el Lado Mayor:");
+                var LadoMayor = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese el Lado Menor:");
+                var LadoMenor = int.Parse(Console.ReadLine());
+                if ( LadoMenor > 0 & LadoMayor > 0)
+                {
+                    rectangulo.LadoMayor = LadoMenor;
+                    rectangulo.LadoMenor = LadoMayor;
+                    
+
+                    Console.WriteLine($"El perimetro es {rectangulo.GetPerimetro()}");
+                    Console.WriteLine($"La superficie es {rectangulo.GetSuperficie()}");
+
+                }
+                else
+                {
+                    
+                    
+                    Console.WriteLine("Ingrese un numero mayor a 0");
+
+                }
+
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Lado mal ingresado");
+                
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Ejercicios01Practico02POO.BL/Rectangulo.cs
@@ -0,0 +1,20 @@
+﻿namespace Ejercicios01Practico02POO.BL
+{
+    public class Rectangulo
+    {
+        public double LadoMayor { get; set; }
+        public double LadoMenor { get; set; }
+        public Rectangulo()
+        {
+
+        }
+        public double GetPerimetro()
+        {
+            return LadoMayor * 2 + LadoMenor * 2;
+        }
+        public double GetSuperficie()
+        {
+            return LadoMayor * LadoMenor;
+        }
+    }
+}
